d3ad73b920ef26e10ffa89b628f3403019ce3314
TRex-Dino <dv.menkov@tensor.rudv.menkov@tensor.rudv.menkov@tensor.ru>
2023-01-30 20:35:46 +0400

    start project3

---
+++ 100days-HWS/Project3/Project3/Project3App.swift
@@ -0,0 +1,17 @@
+//
+//  Project3App.swift
+//  Project3
+//
+//  Created by Меньков Д.В. on 30/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Project3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
